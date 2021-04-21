9780d34a2f3dbc74931271979ff5a02fbfad9ee9
Dakota Lillie <dakota.lillie@icloud.com>
2021-04-20 23:06:05 -0700

    Update

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
+variable "byte_length" {
+  default     = 8
+  description = "The byte length of the random ID"
+  type        = number
+}
+
 resource "random_id" "main" {
   byte_length = 8
 }
 
 output "id" {
-  value = random_id.main.dec
+  description = "A randomly generated ID"
+  value       = random_id.main.dec
 }
